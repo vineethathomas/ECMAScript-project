592d08fe7edc67245e4085287e0e52148ba7dbb0
Vineetha Thomas <connecttovineetha86@gmail.com>
2020-10-11 18:30:15 +1000

    first commit

---
+++ es6-course-master/app/files/arrayDestructuring.js
@@ -0,0 +1,8 @@
+const names = ['Anna', 'Mariam', 'Joe', 'Mark', 'Matt'];
+
+const name1 = names[0];
+const name2 = names[1];
+const name3 = names[2];
+
+log(`${name1} ${name2} ${name3}`);
+
---
+++ es6-course-master/app/js/Animal.js
@@ -0,0 +1,10 @@
+export default class Animal{
+
+    constructor() {
+        console.log("This is animal class")
+    }
+    getClassType() {
+        return "Animal";
+    }
+
+}
